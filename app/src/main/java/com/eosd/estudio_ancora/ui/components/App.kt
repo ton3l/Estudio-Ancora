f1b2b3d201044c86c58b09f2c45fdb0b8625f53e
Elton <elton.felype.s.nascimento@gmail.com>
2026-02-08 15:53:30 -0300

    fix: fix nav component UI and add it to App Scaffold, fix BookingLog behavior

--- app/src/main/java/com/eosd/estudio_ancora/ui/components/App.kt
+++ app/src/main/java/com/eosd/estudio_ancora/ui/components/App.kt
@@ -1,12 +1,16 @@
 package com.eosd.estudio_ancora.ui.components
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import com.eosd.estudio_ancora.ui.screens.BookingForm
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.eosd.estudio_ancora.ui.screens.BookingDateSelectScreen
 import com.eosd.estudio_ancora.ui.screens.BookingLog
 
 @Composable
@@ -14,17 +18,34 @@ fun App() {
     Scaffold(
         topBar = {
             AppHeader(modifier = Modifier)
-        }
+        },
+        bottomBar = {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = androidx.compose.foundation.layout.Arrangement.End
+            ) {
+                Nav(
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .padding(bottom = 16.dp)
+                )
+            }
+        },
+        containerColor = Color.Transparent
     ) { innerPadding ->
-        val mod = Modifier
-                .padding(innerPadding)
-                .padding(horizontal = 16.dp, vertical = 32.dp);
-        BookingForm(
-            modifier = mod
+        BookingLog(
+            modifier = Modifier
+                .padding(top = innerPadding.calculateTopPadding())
+                .padding(top = 16.dp)
+                .padding(horizontal = 16.dp),
         )
-//        BookingLog(
-//            modifier = mod
+//        BookingForm(
+//            modifier = Modifier
+//                .padding(innerPadding)
+//                .padding(horizontal = 16.dp, vertical = 32.dp)
 //        )
+//        BookingDateSelectScreen() { }
     }
 }
 
--- app/src/main/java/com/eosd/estudio_ancora/ui/components/Nav.kt
+++ app/src/main/java/com/eosd/estudio_ancora/ui/components/Nav.kt
@@ -1,6 +1,8 @@
 package com.eosd.estudio_ancora.ui.components
 
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CalendarMonth
@@ -10,13 +12,13 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SegmentedButton
 import androidx.compose.material3.SegmentedButtonDefaults
 import androidx.compose.material3.SingleChoiceSegmentedButtonRow
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -26,44 +28,47 @@ fun Nav(modifier: Modifier = Modifier) {
     var selectedIndex by remember { mutableIntStateOf(0) }
     val options = listOf("Calendar", "BookingList")
 
-    SingleChoiceSegmentedButtonRow(
-        modifier = modifier
-            .height(64.dp)
-            .shadow(
-                elevation = 2.dp,
-                shape = CircleShape
-            )
+    Surface (
+        shadowElevation = 2.dp,
+        modifier = modifier,
+        shape = CircleShape
     ) {
-        options.forEachIndexed { index, label ->
-            SegmentedButton(
-                shape = SegmentedButtonDefaults.itemShape(
-                    index = index,
-                    count = options.size,
-                ),
-                onClick = { selectedIndex = index },
-                selected = index == selectedIndex,
-                label = {
-                    when (label) {
-                        "Calendar" -> Icon(
-                            imageVector = Icons.Default.CalendarMonth,
-                            contentDescription = "Calendar"
-                        )
-                        "BookingList" -> Icon(
-                            imageVector = Icons.Default.Menu,
-                            contentDescription = "Menu"
-                        )
-                    }
-                },
-                icon = {},
-                colors = SegmentedButtonDefaults.colors(
-                    activeContainerColor = MaterialTheme.colorScheme.secondaryContainer,
-                    activeContentColor = MaterialTheme.colorScheme.onSecondaryContainer,
-                    activeBorderColor = Color.Transparent,
-                    inactiveBorderColor = Color.Transparent
-                ),
-                modifier = modifier
-                    .height(57.8.dp)
-            )
+        SingleChoiceSegmentedButtonRow(
+            modifier = Modifier
+                .height(56.dp)
+        ) {
+            options.forEachIndexed { index, label ->
+                SegmentedButton(
+                    shape = SegmentedButtonDefaults.itemShape(
+                        index = index,
+                        count = options.size,
+                    ),
+                    onClick = { selectedIndex = index },
+                    selected = index == selectedIndex,
+                    label = {
+                        when (label) {
+                            "Calendar" -> Icon(
+                                imageVector = Icons.Default.CalendarMonth,
+                                contentDescription = "Calendar"
+                            )
+
+                            "BookingList" -> Icon(
+                                imageVector = Icons.Default.Menu,
+                                contentDescription = "Menu"
+                            )
+                        }
+                    },
+                    icon = {},
+                    colors = SegmentedButtonDefaults.colors(
+                        activeContainerColor = MaterialTheme.colorScheme.secondaryContainer,
+                        activeContentColor = MaterialTheme.colorScheme.onSecondaryContainer,
+                        activeBorderColor = Color.Transparent,
+                        inactiveBorderColor = Color.Transparent
+                    ),
+                    modifier = Modifier
+                        .fillMaxHeight()
+                )
+            }
         }
     }
 }
@@ -71,5 +76,5 @@ fun Nav(modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun NavPreview() {
-    Nav()
+    Nav(Modifier.padding(16.dp))
 }
